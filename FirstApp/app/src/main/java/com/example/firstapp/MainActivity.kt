3e5263e99c669f08ddd006c725803eaa0c88cddd
deepalexkono <konopelko99@gmail.com>
2020-02-21 18:41:23 +0300

    First task

--- FirstApp/app/src/main/java/com/example/firstapp/MainActivity.kt
+++ FirstApp/app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.firstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainButton = main_button_1
+        mainButton.setOnClickListener {
+            val newIntent = Intent(this, FirstTaskActivity::class.java)
+            startActivity(newIntent)
+        }
     }
 }
